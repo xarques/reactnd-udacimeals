a4399659104357fb7fc653f6926fdc3a9e7b7bdb
Xavier Arques <xavierarques@yahoo.com>
2018-03-11 22:16:57 +0100

    Add a second reducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import { connect} from 'react-redux';
+import { addRecipe, removeFromCalendar } from '../actions';
+
 class App extends Component {
+  doThing = () => {
+    this.props.selectRecipe({});
+  }
   render() {
     console.log("Props : " , this.props)
     return (
@@ -26,4 +31,9 @@ const mapStateToProps = calendar => {
     }))
   }
 }
-export default connect(mapStateToProps)(App)
+
+const mapDispatchToProps = dispatch => ({
+  selectRecipe: data => dispatch(addRecipe(data)),
+  remove: data => dispatch(removeFromCalendar(data))
+})
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,20 @@
-import { ADD_RECIPE, REMOVE_FROM_CALENDAR } from '../actions';
+import { 
+  ADD_RECIPE, 
+  REMOVE_FROM_CALENDAR 
+} from '../actions';
+
+const food = (state = {}, action) => {
+  switch (action.type) {
+    case ADD_RECIPE:
+      const { recipe } = action;
+      return {
+        ...state,
+        [recipe.label]: recipe
+      }
+    default:
+      return state;
+  }
+}
 
 const initialCalendarState = {
   monday: {
